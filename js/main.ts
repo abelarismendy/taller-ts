ca30c168658ae21389013b1eeb6e7df3f84579bb
Abel Arismendy <a.arismendy@uniandes.edu.co>
2023-04-16 20:39:38 -0500

    TS listo, parte 1

---
+++ js/main.ts
@@ -0,0 +1,31 @@
+import { series } from "./data";
+
+const tableBody = document.createElement("tbody");
+
+series.forEach((serie) => {
+    const row = document.createElement("tr");
+    const idCell = document.createElement("td");
+    const nameCell = document.createElement("td");
+    const channelCell = document.createElement("td");
+    const seasonsCell = document.createElement("td");
+
+    // Create an anchor element for the title with the link
+    const nameLink = document.createElement("a");
+    nameLink.href = serie.link;
+    nameLink.textContent = serie.name;
+    nameLink.target = "_blank";
+
+    idCell.textContent = serie.id.toString();
+    nameCell.appendChild(nameLink);
+    channelCell.textContent = serie.channel;
+    seasonsCell.textContent = serie.seasons.toString();
+
+    row.appendChild(idCell);
+    row.appendChild(nameCell);
+    row.appendChild(channelCell);
+    row.appendChild(seasonsCell);
+    tableBody.appendChild(row);
+});
+
+const tableElement = document.querySelector("table") ?? document.createElement("table");
+tableElement.appendChild(tableBody);
